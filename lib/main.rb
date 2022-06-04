cec2921793db257e8fb3816536a55ada920f775c
Malcolm XubuntuAir <malcolmangsc@gmail.com>
2022-06-04 18:08:15 +0700

    Started chess - create empty board

---
+++ lib/main.rb
@@ -0,0 +1,91 @@
+LETTER_TO_NUM = {'a'=> '1', 'b' => '2', 'c' => '3', 'd' => '4', 'e' => '5', 'f' => '6',
+'g' => '7', 'h' => '8'}
+
+module Colour
+  WHITE = 100
+  BLACK = 40
+end
+
+module CellState
+  DEFAULT = 0
+  POSSIBLE_PUSH = 46
+end
+
+class Board
+  def initialize
+    @board = createboard()
+    printboard()
+  end
+
+  private
+  def createboard
+    buffer_map = Hash.new()
+    for number in ('1'..'8').reverse_each() do #the printing order - row first, then column, but rows are reversed
+      for letter in 'a'..'h' do
+        buffer_map[letter+number] = Cell.new(letter,number)
+      end
+    end
+    return buffer_map
+  end
+
+  def printboard()
+    puts "   a  b  c  d  e  f  g  h "
+    for number in ('1'..'8').reverse_each() do #the printing order - row first, then column, but rows are reversed
+      outputstr = "#{number} "
+      for letter in 'a'..'h' do
+        outputstr += @board[letter+number].printcell()
+      end
+      puts outputstr
+    end
+  end
+end
+
+class Cell
+  attr_accessor :piece
+  def initialize(letter,number)
+    @position = letter+number
+    @background_colour = determine_colour(@position)
+    @state = CellState::DEFAULT
+    @piece = nil
+  end
+
+  def printcell()
+    if !@piece
+      string = "   "
+    else
+      pass #placeholder for piece info
+    end
+
+    if @state == CellState::DEFAULT
+      bg_colour = @background_colour
+    else 
+      bg_colour = 100
+    end
+    return "\e[#{@piece_colour};#{bg_colour}m#{string}\e[0m"
+
+  end
+
+  private 
+  def determine_colour(pos)
+    if LETTER_TO_NUM[pos[0]].to_i.even?  #a1 is black (letter not even, number not even)
+      if pos[1].to_i.even?  
+        return Colour::BLACK
+      else 
+        return Colour::WHITE
+      end
+    else 
+      if pos[1].to_i.even?  
+        return Colour::WHITE
+      else 
+        return Colour::BLACK
+      end
+    end
+  end
+end
+
+class Piece
+end
+
+class Rook < Piece
+end
+game = Board.new()
